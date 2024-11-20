a76db858186b50e9dbb120d2c321af0ae877f131
camoverride <camoverride@gmail.com>
2024-11-20 12:20:28 -0800

    fix pi freezing erorr thing

--- stream_video.py
+++ stream_video.py
@@ -14,7 +14,7 @@ class VLCPlayer:
         self.instance = vlc.Instance(
             "--no-audio", "--no-xlib", "--video-title-show",
             "--no-video-title", "--avcodec-hw=none",
-            "--network-caching=5000", "--clock-synchro=0", "--file-caching=5000",
+            "--network-caching=10000", "--clock-synchro=0", "--file-caching=10000",
             "--ts-seek-percent", "--sout-ts-shaping=1"
         )
         self.player = self.instance.media_player_new()
