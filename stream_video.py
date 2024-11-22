27fe7fcbbfb1fae4088f3af2163708644f81552e
camoverride <camoverride@gmail.com>
2024-11-22 15:01:34 -0800

    camera offline, add new one

--- stream_video.py
+++ stream_video.py
@@ -66,6 +66,7 @@ def compute_frame_hash(frame):
 
 def main():
     url = "https://61e0c5d388c2e.streamlock.net/live/2_Lenora_NS.stream/chunklist_w165176739.m3u8"
+    url = "https://61e0c5d388c2e.streamlock.net/live/2_Virginia_NS.stream/chunklist_w155634764.m3u8"
     retry_delay = 5  # Seconds to wait before restarting after an error
 
     while True:
