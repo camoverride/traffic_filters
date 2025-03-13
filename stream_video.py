c59070846de8a0aaea65dfba86310affbdf1da0c
camoverride <camoverride@gmail.com>
2025-03-13 16:06:52 -0700

    force cv2 to display fullscreen

--- stream_video.py
+++ stream_video.py
@@ -106,8 +106,11 @@ if __name__ == "__main__":
     player.start()
 
     # Set up the cv2 display window.
-    cv2.namedWindow("Video Stream", cv2.WINDOW_NORMAL)
+    # cv2.namedWindow("Video Stream", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("Video Stream", cv2.WINDOW_FREERATIO)
     cv2.setWindowProperty("Video Stream", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.waitKey(1)
+    os.system("wmctrl -r 'Video Stream' -b add,fullscreen")
 
     # Set up frame hashing to track whether the stream is frozen.
     current_frame_hash = None
