cf727dda95e617f1a2f34d8304d399d44324c34c
camoverride <camoverride@gmail.com>
2025-05-09 18:54:18 -0700

    ubuntu

--- stream_video.py
+++ stream_video.py
@@ -23,8 +23,9 @@ class VLCPlayer:
         self.width, self.height = 1024, 768
         self.instance = vlc.Instance(
             "--no-audio", "--no-xlib", "--file-caching=5000", "--network-caching=5000",
-            "--avcodec-hw=any", "--verbose=1", "--logfile=vlc_log.txt"
-        )
+            "--no-hw-decoding", "--verbose=1", "--logfile=vlc_log.txt"
+)
+
         self.player = self.instance.media_player_new()
         self.frame_data = np.zeros((self.height, self.width, 4), dtype=np.uint8)
         self.frame_pointer = self.frame_data.ctypes.data_as(ctypes.POINTER(ctypes.c_uint8))
