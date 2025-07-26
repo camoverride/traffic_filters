09c6f7ebb4731ce1f09c9e54462e83bea1ba14a4
camoverride <camoverride@gmail.com>
2025-07-25 20:09:23 -0700

    debug

--- stream_video2.py
+++ stream_video2.py
@@ -122,7 +122,7 @@ def main():
                     frame = np.flipud(player.frame.copy())
 
                     # Get correct rotation.
-                    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+                    frame = cv2.rotate(frame, cv2.ROTATE_180)
 
                     # Get into correct model format.
                     frame = np.ascontiguousarray(frame)
