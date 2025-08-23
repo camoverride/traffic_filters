e3eb4fb4e376e87af899e5faa1b72beaf48cc726
camoverride <camoverride@gmail.com>
2025-08-22 18:05:41 -0700

    try/except catch everything

--- show_images.py
+++ show_images.py
@@ -11,33 +11,36 @@ with open("config.yaml", "r") as f:
 folder = 'frames/'
 
 while True:
-    # Get list of files in folder
-    files = os.listdir(folder)
-    if not files:
-        print("No images found.")
-        time.sleep(0.00001)
-        continue
+    try:
+        # Get list of files in folder
+        files = os.listdir(folder)
+        if not files:
+            print("No images found.")
+            time.sleep(0.00001)
+            continue
 
-    # Get latest file by creation time
-    latest_file = max(files, key=lambda x: os.path.getctime(os.path.join(folder, x)))
+        # Get latest file by creation time
+        latest_file = max(files, key=lambda x: os.path.getctime(os.path.join(folder, x)))
 
-    # Read and show image
-    image = cv2.imread(os.path.join(folder, latest_file))
-    if image is None:
-        print("Failed to load image.")
-        time.sleep(0.00001)
-        continue
+        # Read and show image
+        image = cv2.imread(os.path.join(folder, latest_file))
+        if image is None:
+            print("Failed to load image.")
+            time.sleep(0.00001)
+            continue
 
 
-    frame = draw_bbs(frame=image,
-                                 target_classes=config["target_classes"],
-                                 bb_color=config["bb_color"],
-                                 draw_labels=config["draw_labels"],
-                                 conf_threshold=config["conf_threshold"])
-    
-    cv2.imshow('Image Window', frame)
+        frame = draw_bbs(frame=image,
+                                    target_classes=config["target_classes"],
+                                    bb_color=config["bb_color"],
+                                    draw_labels=config["draw_labels"],
+                                    conf_threshold=config["conf_threshold"])
+        
+        cv2.imshow('Image Window', frame)
 
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
 
+    except:
+        pass
 cv2.destroyAllWindows()
